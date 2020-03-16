bd5c6b2d5566e5e53a6af2b80fb541b81340bca3
AI1411 <goethe0719@gmail.com>
2020-03-16 23:53:58 +0900

    filterlaodを追加

---
+++ pkg/protocol/message/filterload.go
@@ -0,0 +1,64 @@
+package message
+
+import (
+	"block_chain_go/pkg/protocol/common"
+	"block_chain_go/pkg/util"
+	"bytes"
+	"encoding/binary"
+	"github.com/spaolacci/murmur3"
+	"math"
+)
+
+type Filterload struct {
+	Count      *common.VarInt
+	Filter     []byte
+	NHashFuncs uint32
+	NTweak     uint32
+	NFlags     uint8
+}
+
+func NewFilterload(size uint32, nHashfuncs uint32, queries [][]byte) *Filterload {
+	byteArray := make([]byte, size)
+	nTweak := make([]byte, 4)
+	for i := 0; i < cap(nTweak); i++ {
+		nTweak[i] = util.RandInt8(0, math.MaxUint8)
+	}
+	nTweakUint32 := binary.BigEndian.Uint32(nTweak)
+	for _, query := range queries {
+		for i := 0; uint32(i) < nHashfuncs; i++ {
+			seed := uint32(i)*0xFBA4C795 + nTweakUint32
+			hashValue := murmur3.Sum32WithSeed(query, seed)
+			adjustHashValue := hashValue % (size * uint32(8))
+			idx := 1 << (uint32(7) & hashValue)
+			value := 1 << (uint32(7) & hashValue)
+			byteArray[idx] = byte(value)
+		}
+	}
+	return &Filterload{
+		Count:      common.NewVarInt(uint64(size)),
+		Filter:     byteArray,
+		NHashFuncs: nHashfuncs,
+		NTweak:     nTweakUint32,
+		NFlags:     uint8(1),
+	}
+}
+
+func (f *Filterload) CommandName() [12]byte {
+	var commandName [12]byte
+	copy(commandName[:], "filterload")
+	return commandName
+}
+
+func (f *Filterload) Encode() []byte {
+	nHashFuncsBytes := make([]byte, 4)
+	nTweakByte := make([]byte, 4)
+	binary.LittleEndian.PutUint32(nHashFuncsBytes, f.NHashFuncs)
+	binary.LittleEndian.PutUint32(nTweakByte, f.NHashFuncs)
+	return bytes.Join([][]byte{
+		f.Count.Encode(),
+		f.Filter,
+		nHashFuncsBytes,
+		nTweakByte,
+		[]byte{f.NFlags},
+	}, []byte{})
+}
--- pkg/util/hash.go
+++ pkg/util/hash.go
@@ -4,6 +4,8 @@ import (
 	"crypto/sha256"
 	"golang.org/x/crypto/ripemd160"
 	"io"
+	"math/rand"
+	"time"
 )
 
 func Hash256(b []byte) []byte {
@@ -17,4 +19,9 @@ func Hash160(b []byte) []byte {
 	rip := ripemd160.New()
 	io.WriteString(rip, string(sum[:]))
 	return rip.Sum(nil)
+}
+
+func RandInt8(min int, max int) uint8 {
+	rand.Seed(time.Now().UTC().UnixNano())
+	return uint8(min + rand.Intn(max-min))
 }
