1e7663db6ad433febf8b07e4fd7ddea162dcc39b
AI1411 <goethe0719@gmail.com>
2020-03-17 23:21:22 +0900

    refactaring

--- cmd/main.go
+++ cmd/main.go
@@ -7,38 +7,12 @@ import (
 	"block_chain_go/pkg/protocol/message"
 	"bytes"
 	"log"
-	"time"
 )
 
 func main() {
 	c := client.NewClient("testnet-seed.bitcoin.jonasschnelli.ch:18333")
 	defer c.Conn.Close()
 	log.Printf("remote addr: %s", c.Conn.RemoteAddr().String())
-
-	addrFrom := &common.NetworkAddress{
-		Services: uint64(1),
-		IP: [16]byte{
-			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xFF, 0xFF, 0x7F, 0x00, 0x00, 0x01,
-		},
-		Port: 8333,
-	}
-
-	v := &message.Version{
-		Version:     uint32(70015),
-		Services:    uint64(1),
-		Timestamp:   uint64(time.Now().Unix()),
-		AddrRecv:    addrFrom,
-		AddrFrom:    addrFrom,
-		Nonce:       uint64(0),
-		UserAgent:   common.NewVarStr([]byte("")),
-		StartHeight: uint32(0),
-		Relay:       false,
-	}
-
-	_, err := c.SendMessage(v)
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	wallet := wallet.NewWallet(c)
 	wallet.Handshake()
@@ -75,7 +49,6 @@ func main() {
 			ping := message.DecodePing(b)
 			pong := message.Pong{Nonce:ping.Nonce}
 			wallet.Client.SendMessage(&pong)
-			wallet.Client.ReceiveMessage(msg.Length)
 		} else if bytes.HasPrefix(msg.Command[:], []byte("addr")) {
 			log.Printf("receive addr")
 			wallet.Client.ReceiveMessage(msg.Length)
